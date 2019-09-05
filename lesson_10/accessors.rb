fa043ce004fd58c7e0827b6b73429e7d5581862c
Andrew -Causelof <akozlov@rocketmail.com>
2019-09-05 22:55:41 +0300

    Added forgotten strong_attr_accessor method

--- lesson_10/accessors.rb
+++ lesson_10/accessors.rb
@@ -21,20 +21,30 @@ module Accessors
   end
 
   def strong_attr_accessor(name, type)
-    raise 'Не совпадает класс!!!' unless name.is_a?(type)
+    raise 'значение не символ' unless name.is_a? Symbol
 
+    attr_reader name
+    define_method("#{name}=") do |v|
+      raise 'Не совпадает класс!!!' unless v.is_a?(type)
+
+      instance_variable_set("@#{name}", v)
+    end
   end
 end
 
-# class Test
-#   extend Accessors
-#   attr_accessor_with_history :smth, :smthelse, :smthmore
-# end
-# test = Test.new
-# test.smth = 1
-# test.smth = 3
-# test.smth = 5
-# test.smthelse = 2
-# test.smthmore = 3
+ class Test
+   extend Accessors
+   attr_accessor_with_history :smth, :smthelse, :smthmore
+   strong_attr_accessor :first, String
+    @frist = String.new
+ end
+ test = Test.new
+ test.smth = 1
+ test.smth = 3
+ test.smth = 5
+ test.smthelse = 2
+ test.smthmore = 3
 # puts test.smth
-# puts test.smth_history
+ puts test.smth_history
+ test.first = 'lets try it out'
+ puts test.first
